596a8ecbf30e9270b56e0acd698a0c432ebace8e
anonymme1 <zyarsln0@gmail.com>
2020-12-17 18:31:05 +0300

    mystd::string

---
+++ myStdString.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+namespace myStd {
+class string {
+	public:
+		char str[50];
+		int num;
+
+		string() { num = 0; str[0] = '\0'; }
+
+		int size() {
+			num=0;
+			for(int i=0;str[i] != '\0';i++) {
+				num++;
+			}
+			return num;
+		}
+
+		void push_back(char a) {
+			str[size()] = a;
+		}
+
+};
+};
+
+std::istream &operator >>(std::istream &i, myStd::string &s) {
+	return i>>s.str;
+}
+
+std::ostream &operator <<(std::ostream &i, myStd::string &s) {
+	return i<<s.str;
+}
+
+int main() {
+	myStd::string str;
+
+	std::cin>>str;
+	str.push_back('x');
+
+	std::cout<<str.size();
+	std::cout<<"\n"<<str<<"\n";
+}
