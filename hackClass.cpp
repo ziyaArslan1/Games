849d51d8d8d40d4eb7d2a2f7a093202111c39935
anonymme1 <zyarsln0@gmail.com>
2020-12-20 05:14:33 +0300

    update

--- hackClass.cpp
+++ hackClass.cpp
@@ -4,25 +4,32 @@ Pointerlar ile sinifin private uyelerinin degerini degistirme
 
 
 #include <iostream>
+#include <string>
 
 class Myobj {
     private:
         int data;
+		char name[15];
     public:
-        Myobj(int data) {
+        Myobj(int data, char *name) {
             this->data = data;
+			strcpy(this->name, name);
         }
 
-        void print() { std::cout<<data<<std::endl; }
+        void print() {
+			std::cout<<data<<std::endl;
+			std::cout<<name<<std::endl;
+		}
 };
 
 int main() {
-    Myobj obj(5);
+    Myobj obj(5, "str");
     obj.print();
     int *ptr = (int*)(&obj); // sinifin adresini int tipinden bi pointera atiyoruz
     *ptr = 100; // pointerin degerini degistirdigimizde gosterdigi adreste otomatik olarak degisiyo :)
-    obj.print();
-	*ptr = 1500;
+
+	char *Cptr = (char*)(&obj);
+	strcpy((char*)&*(Cptr+4), "hiii");
 	obj.print();
 
 	return 0;
