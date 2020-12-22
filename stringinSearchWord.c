5031d9450e6e66bc469c805db990318d66dd35e6
anonymme1 <zyarsln0@gmail.com>
2020-12-22 22:47:58 +0300

    search

---
+++ stringinSearchWord.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int search(char *string, char *word) {
+	int len = strlen(word);
+	int count=0, flag;
+
+	for(int i=0;i<strlen(string)-1;i++) {
+		flag=0;
+
+		char *temp = (char*)malloc(sizeof(char)*len+1);
+
+		for(int j=i;j<strlen(string);j++) {
+			if(len+1 == flag) { break; }
+			temp[flag++] = string[j];
+
+		}
+		if(temp[len] == ' ') { temp[len] = '\0'; }
+		if(strcmp(temp, word) == 0) { count++; }
+	}
+
+	return count;
+}
+
+int main() {
+	printf("Bir >> %d\n", search("bir berber bir berbere gel beraber bir berber dukkani acalim demis", "bir"));
+	printf("Berber >> %d\n", search("bir berber bir berbere gel beraber bir berber dukkani acalim demis", "berber"));
+
+	return 0;
+}
