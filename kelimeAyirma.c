72ff2d2284d56d96b22c4103c1772e83e8306dc5
ziyaArslan1 <zyarsln0@gmail.com>
2021-01-04 20:06:58 +0300

    first commit

--- kelimeAyirma.c
+++ kelimeAyirma.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int myStrlen(char *str) {
 	int num=0;
@@ -27,22 +28,28 @@ int main() {
 	char arr[10][30]={0};
 	char str[200]={0};
 
+	printf("\nCumle gir(sadece 200 karakter): ");
 	fgets(str, sizeof(str), stdin);
+
 	int y=0;
 
+	printf("\nKelimeler ayriliyor....\n");
+	sleep(1);
+
 	for(int i=0;i<myStrlen(str);) {
-		int j;
+		int j, x=0;
 		for(j=i;str[j] != ' ';j++);
 
 		char *temp = (char*)malloc(sizeof(char)*j);
-		int x=0;
 
 		for(j=i;str[i++] != ' ';j++) {
 			temp[x++] = str[j];
 		}
 
 		myStrcpy(arr[y++], temp);
+		free(temp);
 	}
+
 	print(arr);
 	fflush(stdin);
 	fflush(stdout);
