dac7044aa262f8708c89f55ae41af842b7b4f0fa
ziyaArslan1 <zyarsln0@gmail.com>
2021-03-23 23:09:23 +0300

    uodate

--- genericMacro.c
+++ genericMacro.c
@@ -3,35 +3,39 @@
 
 #define DA_DEF(TYPE) \
 typedef struct TYPE##opt { \
-        TYPE *arr; \
+        TYPE *array; \
         size_t capacity; \
         size_t size; \
+		char type[10]; \
 }TYPE##_opt; \
-void init(TYPE##_opt *res) { \
+void init(TYPE##_opt *res, char *type) { \
 	res->capacity = 4;\
 	res->size = 0;\
-	res->arr = (TYPE*)malloc(sizeof(TYPE)*res->capacity);\
+	res->array = (TYPE*)malloc(sizeof(TYPE)*res->capacity);\
+	strcpy(res->type, type); \
 }\
-TYPE##_add(TYPE##_opt * res, TYPE value) {\
-		if(res->size < res->capacity) \
-			res->arr[res->size++] = value;\
-		else\
-			printf("array is full\n");\
+void add(TYPE##_opt * res, TYPE value) {\
+	if(res->size < res->capacity) {\
+		res->array[res->size++] = value;\
+	}else{\
+		res->array = (TYPE*)realloc(res->array, sizeof(TYPE)*res->capacity*2);\
+		res->capacity *= 2;\
+	}\
 }
 
 DA_DEF(int); // DA_DEF(int) -> TYPE = int
 
 int main() {
 	int_opt *x = (int_opt*)malloc(sizeof(int_opt));
-	init(x);
+	init(x, "int");
 
-	for(int i=0;i<5;i++)
-		int_add(x, i);
+	for(int i=0;i<10;i++)
+		add(x, i);
 
 	for(size_t i=0;i<x->size;i++)
-		printf("%d\n", x->arr[i]);
+		printf("%d\n", x->array[i]);
 
-	free(x->arr);
+	free(x->array);
 	free(x);
 
 	return 0;
