9be3791d612620b3905d5be8c478b37cdc35ac31
ziyaArslan1 <zyarsln0@gmail.com>
2021-04-18 23:16:18 +0300

    sample

---
+++ ismizy.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#define o_o std::cout
+#define oOo(x) atoi(x)
+
+typedef int _o_;
+typedef char _oo_;
+
+_o_ main() {
+	_o_ OOo[] = {
+				  353,354,355,356,357,358,
+				  359,360,361,362,362,363,364,
+				  365,366,367,368,369,370,371,
+				  372,373,374,375,376,377,378
+				};
+
+	_o_ oOO[] = {
+				  304, 305, 306, 307, 308,
+				  309, 310, 311, 312, 313
+				};
+
+	o_o<<(_oo_*)&OOo[oOo((_oo_*)&oOO[8])]<<(_oo_*)&OOo[19]<<(_oo_*)&OOo[13];
+	o_o<<(_oo_*)&OOo[oOo((_oo_*)&oOO[8])]<<(_oo_*)&OOo[26]<<(_oo_*)&OOo[25];
+	o_o<<'\n';
+}
