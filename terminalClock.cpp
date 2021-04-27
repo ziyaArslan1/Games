d8a0a53571d7d08a28b4a44145ae274a3b1e5c49
ziyaArslan1 <zyarsln0@gmail.com>
2021-04-27 11:47:06 +0300

    update

--- terminalClock.cpp
+++ terminalClock.cpp
@@ -12,25 +12,23 @@ void watchClock(int hour, int min, int second, char *color) {
 	sprintf(arr[3], "| SECOND >>  %d   |", second);
 	strcpy(arr[4],  "===================");
 
-	if(hour <= 9) {
+	if(hour <= 9)
 		sprintf(arr[1], "| HOUR   >>  0%d   |", hour);
-	} if(min <= 9) {
+	if(min <= 9)
 		sprintf(arr[2], "| MINUTE >>  0%d   |", min);
-	} if(second <= 9) {
+	if(second <= 9)
 		sprintf(arr[3], "| SECOND >>  0%d   |", second);
-	}
 
 	std::cout<<std::endl<<std::endl<<std::endl;
 
-	for(int i=0;i<5;i++) {
+	for(int i=0;i<5;i++)
 		std::cout<<color<<"      "<<arr[i]<<std::endl;
-	}
 }
 
 int main() {
 	int hour, min, second=0;
 	char color[] = "\e[94m";
-	char c;
+	unsigned char c, ch=7;
 
 	std::cout<<"\nHour minute (23:59): ";
 	std::cin>>hour>>c>>min;
@@ -54,6 +52,7 @@ int main() {
 
 		watchClock(hour, min, second, color);
 		second++;
+		std::cout<<ch;
 		sleep(1);
 	}
 	return 0;
