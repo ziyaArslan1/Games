d1bafc83a255cead23b91ddd7e845f1fc3aa2b1b
ziyaArslan1 <zyarsln0@gmail.com>
2021-05-10 01:10:56 +0300

    chessGame continues

---
+++ chess.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAP 8
+
+/*
+	1 piyon
+	2 kale
+	3 at
+	4 fil
+	5 vezir
+	6 sah
+*/
+
+/* PROTOTYPE */
+
+void myMove(int (*)[][MAP], const char*, const char*);
+void init(int (*)[][MAP]);
+void printMap(int (*)[][MAP]);
+int ctrlRowCol(const char*, const char*);
+void choice(int (*)[][MAP]);
+int rockCtrl(int (*)[][MAP], const int, const int, const int, const int, const int);
+
+void init(int (*map)[][MAP]) {
+	for(int i=0;i<MAP;i++) {
+		(*map)[1][i] = 1;
+		(*map)[6][i] = 1;
+	}
+
+	(*map)[0][0] = (*map)[0][MAP-1] = 2;
+	(*map)[0][1] = (*map)[0][MAP-2] = 3;
+	(*map)[0][2] = (*map)[0][MAP-3] = 4;
+
+	(*map)[0][3] = 5;
+	(*map)[0][4] = 6;
+
+	(*map)[MAP-1][0] = (*map)[MAP-1][MAP-1] = 2;
+	(*map)[MAP-1][1] = (*map)[MAP-1][MAP-2] = 3;
+	(*map)[MAP-1][2] = (*map)[MAP-1][MAP-3] = 4;
+
+	(*map)[MAP-1][3] = 5;
+	(*map)[MAP-1][4] = 6;
+}
+
+void printMap(int (*map)[][MAP]) {
+	printf("   ");
+	for(int i=1;i<MAP+1;i++)
+		printf("%d   ", i);
+	printf("\n  ");
+	for(int i=0;i<MAP*2;i++)
+		printf("\e[91m__");
+	printf("\n");
+
+	char chars = 97;
+	for(int i=0;i<MAP;i++) {
+		printf("\e[91m%c  ", chars++);
+		for(int j=0;j<MAP;j++) {
+			if((*map)[i][j] == 0)
+				printf("\e[90m%d | ", (*map)[i][j]);
+			else
+				printf("\e[93m%d | ", (*map)[i][j]);
+		}
+		printf("\n");
+	}
+	printf("  ");
+	for(int i=0;i<MAP*2;i++)
+		printf("\e[91m__");
+	printf("\n\n");
+}
+
+int ctrlRowCol(const char *val1, const char *val2) { return ((strlen(val1) == 2 && strlen(val2) == 2) && ((val1[0] >= 97 && val1[0] <= 104) && (val2[0] >= 97 && val2[0] <= 104)) && ((val1[1] >= 49 && val1[1] <= 56) && (val2[1] >= 49 && val2[1] <= 56))); }
+
+void choice(int (*map)[][MAP]) {
+	char first[3], second[3];
+
+	while(1) {
+		printf("tas sec: ");
+		scanf("%s", first);
+		printf("gidcegi yer: ");
+		scanf("%s", second);
+
+		if(!ctrlRowCol(first, second))
+			printf("Hatali secim\n");
+		else
+			break;
+	}
+
+	myMove(map, first, second);
+}
+
+void myMove(int (*map)[][MAP], const char *source, const char *target) {
+	int sourceRow = source[0]-(48*2)-1, sourceCol = source[1]-48-1;
+	int targetRow = target[0]-(48*2)-1, targetCol = target[1]-48-1;
+
+	if((*map)[sourceRow][sourceCol] == 0) {
+		printf("\norda tas yok .d\n");
+		return;
+	}
+
+	if(!rockCtrl(map,sourceRow, sourceCol, (*map)[sourceRow][sourceCol], targetRow, targetCol)) {
+		printf("oraya gidemezsin.\n");
+		return;
+	}
+
+	(*map)[targetRow][targetCol] = (*map)[sourceRow][sourceCol];
+	(*map)[sourceRow][sourceCol] = 0;
+}
+
+int rockCtrl(int (*map)[][MAP], const int rockRow, const int rockCol, const int rock, const int row, const int col) {
+	if(rock == 1) {
+		if(((rockRow - row) == -1 || (rockRow - row) == 1) && ((*map)[row][col] == 0))
+			return 1;
+	}
+
+	return 0;
+}
+
+int main() {
+	int map[MAP][MAP]={0};
+
+	init(&map);
+	printMap(&map);
+
+	while(1) {
+	choice(&map);
+	printMap(&map);
+}
+}
